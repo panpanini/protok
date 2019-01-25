db8d4a3fe5c7ad01f3e3b462fdcefd15abf38afc
panpanini <panpanini@mercari.com>
2019-01-25 15:10:34 +0900

    Handle messages with no fields

--- src/main/kotlin/jp/co/panpanini/MessageGenerator.kt
+++ src/main/kotlin/jp/co/panpanini/MessageGenerator.kt
@@ -303,7 +303,7 @@ class MessageGenerator(private val file: File, private val kotlinTypeMappings: M
         //TODO: clean this up - its a little difficult to follow. maybe create a function for it
         codeBlock.beginControlFlow("while (true)")
         codeBlock.beginControlFlow("when (${unMarshalParameter.name}.readTag())")
-                .addStatement("0 -> ${typeName.simpleName}(${doneKotlinFields.joinToString()}, ${unMarshalParameter.name}.unknownFields())")
+                .addStatement("0 -> ${typeName.simpleName}(${doneKotlinFields.joinToString()}${if(doneKotlinFields.isNotEmpty()) ", " else ""}${unMarshalParameter.name}.unknownFields())")
 
         type.sortedStandardFieldsWithOneOfs().map { (field, oneOf) ->
             val tags = mutableListOf(field.tag)
