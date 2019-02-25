3deaddc3516b4d98dd86e6a04c615a653e99110d
panpanini <panpanini@mercari.com>
2019-02-26 08:37:53 +0900

    ♻️ Hide implementation of unmarshal(inputStream) inside message

--- runtime/src/main/kotlin/Message.kt
+++ runtime/src/main/kotlin/Message.kt
@@ -1,11 +1,14 @@
 package jp.co.panpanini
 
+import com.google.protobuf.CodedInputStream
 import pbandk.Message
 import pbandk.Unmarshaller
+import java.io.InputStream
 
 interface Message<T : jp.co.panpanini.Message<T>> : Message<T> {
     fun protoUnmarshal(u: Unmarshaller): T
     fun protoUnmarshal(arr: ByteArray) = protoUnmarshal(Unmarshaller.fromByteArray(arr))
+    fun protoUnmarshal(inputStream: InputStream) = protoUnmarshal(Unmarshaller(CodedInputStream.newInstance(inputStream)))
 
     interface Companion<T : jp.co.panpanini.Message<T>> : Message.Companion<T>
 }
