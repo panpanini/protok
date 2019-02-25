2ae5cb43ba654641844a319983b5f4c08ea3cb12
panpanini <panpanini@mercari.com>
2019-02-26 08:39:25 +0900

    ♻️ use provided unmarshal(byteStream) function

--- retrofit-converter/src/main/kotlin/jp/co/panpanini/ProtokResponseBodyConverter.kt
+++ retrofit-converter/src/main/kotlin/jp/co/panpanini/ProtokResponseBodyConverter.kt
@@ -1,13 +1,11 @@
 package jp.co.panpanini
 
-import com.google.protobuf.CodedInputStream
 import okhttp3.ResponseBody
-import pbandk.Unmarshaller
 import retrofit2.Converter
 
 class ProtokResponseBodyConverter<T : Message<*>>(private val adapter: T) : Converter<ResponseBody, T> {
 
     override fun convert(value: ResponseBody): T? {
-        return adapter.protoUnmarshal(Unmarshaller(CodedInputStream.newInstance(value.byteStream()))) as? T
+        return adapter.protoUnmarshal(value.byteStream()) as? T
     }
 }
