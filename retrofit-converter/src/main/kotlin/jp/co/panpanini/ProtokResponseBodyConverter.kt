3e960addb31ed8f7e5494507209e1f506b15b7d3
panpanini <panpanini@mercari.com>
2019-02-26 12:25:58 +0900

    ♻️ close bytestream after use

--- retrofit-converter/src/main/kotlin/jp/co/panpanini/ProtokResponseBodyConverter.kt
+++ retrofit-converter/src/main/kotlin/jp/co/panpanini/ProtokResponseBodyConverter.kt
@@ -6,6 +6,6 @@ import retrofit2.Converter
 class ProtokResponseBodyConverter<T : Message<*>>(private val adapter: T) : Converter<ResponseBody, T> {
 
     override fun convert(value: ResponseBody): T? {
-        return adapter.protoUnmarshal(value.byteStream()) as? T
+        return value.byteStream().use(adapter::protoUnmarshal) as? T
     }
 }
