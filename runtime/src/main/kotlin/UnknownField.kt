183678e24d3c6faef2e048522f95572d3b6d3dff
panpanini <panpanini@mercari.com>
2019-07-22 19:40:13 +0900

    Add UnknownField

---
+++ runtime/src/main/kotlin/UnknownField.kt
@@ -0,0 +1,52 @@
+package jp.co.panpanini
+
+import pbandk.ByteArr
+import pbandk.Sizer
+
+class UnknownField(val fieldNum: Int, val value: Value) {
+    constructor(fieldNum: Int, value: Long, fixed: Boolean = false) :
+            this(fieldNum, if (fixed) Value.Fixed64(value) else Value.Varint(value))
+    constructor(fieldNum: Int, value: Int, fixed: Boolean = false) :
+            this(fieldNum, if (fixed) Value.Fixed32(value) else Value.Varint(value.toLong()))
+    constructor(fieldNum: Int, value: ByteArr) :
+            this(fieldNum, Value.LengthDelimited(value))
+    constructor(fieldNum: Int, value: ByteArray) :
+            this(fieldNum, Value.LengthDelimited(ByteArr(value)))
+    constructor(fieldNum: Int, value: String) :
+            this(fieldNum, Value.LengthDelimited(ByteArr(value.toByteArray())))
+
+    fun size(): Int {
+        return if (value is Value.Composite) {
+            val tagSize = Sizer.tagSize(fieldNum) * value.values.size
+            tagSize + value.size()
+        } else {
+            Sizer.tagSize(fieldNum) + value.size()
+        }
+    }
+
+    sealed class Value {
+        abstract fun size(): Int
+
+        data class Varint(val varint: Long) : Value() {
+            override fun size() = Sizer.uInt64Size(varint)
+        }
+        data class Fixed64(val fixed64: Long) : Value() {
+            override fun size() = Sizer.fixed64Size(fixed64)
+        }
+        data class LengthDelimited(val bytes: ByteArr) : Value() {
+            override fun size() = Sizer.bytesSize(bytes)
+        }
+        object StartGroup : Value() {
+            override fun size() = TODO()
+        }
+        object EndGroup : Value() {
+            override fun size() = TODO()
+        }
+        data class Fixed32(val fixed32: Int) : Value() {
+            override fun size() = Sizer.fixed32Size(fixed32)
+        }
+        data class Composite(val values: List<Value>) : Value() {
+            override fun size() = values.sumBy { it.size() }
+        }
+    }
+}
