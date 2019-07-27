f2b28148a4bebb2c5ab0de51cefae0be104283eb
panpanini <panpanini@mercari.com>
2019-07-27 15:07:29 +0900

    Add serializable to runtime classes

--- runtime/src/main/kotlin/ByteArr.kt
+++ runtime/src/main/kotlin/ByteArr.kt
@@ -1,6 +1,8 @@
 package jp.co.panpanini
 
-class ByteArr(val array: ByteArray = ByteArray(0)) {
+import java.io.Serializable
+
+class ByteArr(val array: ByteArray = ByteArray(0)) : Serializable {
     override fun equals(other: Any?) = other is ByteArr && array.contentEquals(other.array)
     override fun hashCode() = array.contentHashCode()
     override fun toString() = array.contentToString()
