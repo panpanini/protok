7c009b584f1f4eb63c70c2eabba96429d358c15e
panini <panpanini@mercari.com>
2020-12-10 18:52:14 +0900

    Move ByteArr to common sourceset

--- runtime-lite/src/commonMain/kotlin/jp/co/panpanini/ByteArr.kt
+++ runtime-lite/src/commonMain/kotlin/jp/co/panpanini/ByteArr.kt
@@ -1,7 +1,5 @@
 package jp.co.panpanini
 
-import java.io.Serializable
-
 class ByteArr(val array: ByteArray = ByteArray(0)) : Serializable {
     override fun equals(other: Any?) = other is ByteArr && array.contentEquals(other.array)
     override fun hashCode() = array.contentHashCode()
